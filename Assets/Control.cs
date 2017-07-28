726eb1060e9ab7db608e4f1c99521eedcbe9d3a8
Mads Ronnow <roennowmads@gmail.com>
2017-07-28 16:57:59 +0200

    Initial Commit
    Draggable circle.

---
+++ Assets/Control.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    private Vector3 m_oldMousePosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        m_oldMousePosition = Input.mousePosition;
+        m_oldMousePosition.z = 1.0f;
+    }
+
+    private void OnMouseDrag()
+    {
+        var screenPoint = Input.mousePosition;
+        screenPoint.z = 1.0f; //distance of the plane from the camera
+        transform.position += Camera.main.ScreenToWorldPoint(screenPoint) - Camera.main.ScreenToWorldPoint(m_oldMousePosition);
+
+        m_oldMousePosition = screenPoint;
+    }
+}
